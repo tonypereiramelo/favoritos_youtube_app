e655b13076a89c36d5a04e176c13196e7074e6bd
tonypereiramelo <tonypereiramelo@gmail.com>
2021-06-15 10:17:16 -0300

    favoriteBloc error

--- lib/blocs/favorite_bloc.dart
+++ lib/blocs/favorite_bloc.dart
@@ -7,8 +7,17 @@ import 'package:favoritos_youtube_app/models/video.dart';
 class FavoriteBloc implements BlocBase {
   Map<String, Video> _favorites = {};
 
-  final _favController = StreamController();
+  final _favController = StreamController<Map<String, Video>>();
   Stream get outFav => _favController.stream;
+
+  void toggleFavorites(Video video) {
+    if (_favorites.containsKey(video.id))
+      _favorites.remove(video.id);
+    else
+      _favorites[video.id.toString()] = video;
+
+    _favController.sink.add(_favorites);
+  }
 
   @override
   void addListener(VoidCallback listener) {}
--- lib/widget/video_tile.dart
+++ lib/widget/video_tile.dart
@@ -1,3 +1,5 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:favoritos_youtube_app/blocs/favorite_bloc.dart';
 import 'package:favoritos_youtube_app/models/video.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +9,7 @@ class VideoTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bloc = BlocProvider.getBloc<FavoriteBloc>();
     return Container(
       margin: EdgeInsets.symmetric(vertical: 4),
       child: Column(
@@ -22,25 +25,40 @@ class VideoTile extends StatelessWidget {
           Row(
             children: <Widget>[
               Expanded(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
-                    child: Text(
-                      video.title.toString(),
-                      style: TextStyle(fontSize: 18, color: Colors.white),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
+                      child: Text(
+                        video.title.toString(),
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      video.channel.toString(),
-                      style: TextStyle(fontSize: 16, color: Colors.blueAccent),
+                    Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        video.channel.toString(),
+                        style:
+                            TextStyle(fontSize: 16, color: Colors.blueAccent),
+                      ),
                     ),
-                  ),
-                ],
-              ))
+                  ],
+                ),
+              ),
+              StreamBuilder<Map<String, Video>>(
+                  stream: bloc.outFav,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData)
+                      return IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.star_border),
+                        color: Colors.white,
+                        iconSize: 30,
+                      );
+                    else
+                      return CircularProgressIndicator();
+                  })
             ],
           )
         ],
